0e13512b7fc0e89dd52a012bd7862a9ce2368ae5
Addison Higham <ahigham@instructure.com>
2016-10-13 14:39:57 -0600

    migrate to ts2.0 @types (except for aws-sdk)

--- test/sinonHelper.ts
+++ test/sinonHelper.ts
@@ -1,4 +1,4 @@
-import * as sinon from 'sinon'
+import * as Sinon from 'sinon'
 import * as _ from 'lodash'
 
 export interface ClassStub<T> extends Sinon.SinonStub {
@@ -55,7 +55,7 @@ export class SinonHelper implements Sinon.SinonSandbox {
 }
 
 function newContext(): SinonHelper {
-  let sandbox = sinon.sandbox.create()
+  let sandbox = Sinon.sandbox.create()
   let helper = new SinonHelper
   helper = _.extend(helper, sandbox) as SinonHelper
 
